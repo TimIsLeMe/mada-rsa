124ab8d0ad44b4475b4d524fb3402ff050e6a3bf
LeMeIsTim <tim.oliver.hofmann@gmail.com>
2023-03-26 20:41:36 +0200

    take text from text.txt

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         RSA rsa = new RSA();
         rsa.generateKeyPair();
-        rsa.writeFile("chiffre.txt", rsa.encrypt("Hello there!\nGeneral Kenobi?".toCharArray()));
+        rsa.writeFile("chiffre.txt", rsa.encrypt(rsa.readFile("text.txt")));
         String decryptedMessage = rsa.decrypt(String.valueOf(rsa.readFile("chiffre.txt")));
         System.out.println(decryptedMessage);
     }
